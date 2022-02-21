b735e8997c484da67c3295b808f2b2cf78be9441
Evelyn Tan <evetan.wy@gmail.com>
2022-02-21 22:20:26 +1300

    created the todo list rendering component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Todo from "./Todo";
 
-function TodoList() {
+function TodoList({ todos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list"></ul>
+      {todos.map((todo) => (
+        <Todo text={todo.text} />
+      ))}
     </div>
   );
 }
